0fc667d768ae6dbcb2342a40a8397dd95cbf7f8c
mmrozewsk <mikolaj.mrozewski2000@gmail.com>
2021-12-15 20:50:11 +0100

    menu

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,33 +1,72 @@
 package main;
 import static java.lang.System.*;
 import static java.lang.System.out;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        int arr1[] = {64,25,12,22,11};
-        int arr2[] = {64,25,12,22,11};
-        int arr3[] = {64,25,12,22,11};
-        int arr4[] = {64,25,12,22,11};
-        int n = arr4.length;
 
-        SelectionSort ss = new SelectionSort();
-        BubbleSort bs = new BubbleSort();
-        InsertionSort is = new InsertionSort();
-        QuickSort qs = new QuickSort();
+        Scanner myInput = new Scanner( System.in );
 
-        ss.ReverseSort(arr1);
-        bs.ReverseSort(arr2);
-        is.ReverseSort(arr3);
-        qs.ReverseSort(arr4, 0, n-1);
+        int arr[] = {64,25,12,22,11};
+        int n = arr.length;
 
-        System.out.println("SelectionSort:");
-        ss.printArray(arr1);
-        System.out.println("BubbleSort:");
-        bs.printArray(arr2);
-        System.out.println("InsertionSort:");
-        is.printArray(arr3);
-        System.out.println("QuickSort :");
-        qs.printArray(arr4);
+        System.out.println("Wybierz algorytm z listy:");
+        System.out.println("1. Selection Sort");
+        System.out.println("2. Bubble Sort");
+        System.out.println("3. Insertion Sort");
+        System.out.println("4. Quick Sort");
+        int algorytm = myInput.nextInt();
+        System.out.println("Wybierz kierunek sortowania:");
+        System.out.println("1. Rosnaco");
+        System.out.println("2. Malejaco");
+        int kierunek = myInput.nextInt();
+
+        if(algorytm == 1 && kierunek == 1){
+            SelectionSort ss = new SelectionSort();
+            ss.sort(arr);
+            ss.printArray(arr);
+        }
+
+        if(algorytm == 1 && kierunek == 2){
+            SelectionSort ss = new SelectionSort();
+            ss.ReverseSort(arr);
+            ss.printArray(arr);
+        }
+
+        if(algorytm == 2 && kierunek == 1){
+            BubbleSort bs = new BubbleSort();
+            bs.sort(arr);
+            bs.printArray(arr);
+        }
+
+        if(algorytm == 2 && kierunek == 2){
+            BubbleSort bs = new BubbleSort();
+            bs.ReverseSort(arr);
+            bs.printArray(arr);
+        }
+        if(algorytm == 3 && kierunek == 1){
+            InsertionSort is = new InsertionSort();
+            is.sort(arr);
+            is.printArray(arr);
+        }
+
+        if(algorytm == 3 && kierunek == 2){
+            InsertionSort is = new InsertionSort();
+            is.ReverseSort(arr);
+            is.printArray(arr);
+        }
+        if(algorytm == 4 && kierunek == 1){
+            QuickSort qs = new QuickSort();
+            qs.sort(arr, 0, n-1);
+            qs.printArray(arr);
+        }
+
+        if(algorytm == 4 && kierunek == 2){
+            QuickSort qs = new QuickSort();
+            qs.ReverseSort(arr, 0, n-1);
+            qs.printArray(arr);
+        }
     }
 }
